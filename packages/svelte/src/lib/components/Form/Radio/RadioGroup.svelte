1e5c650cdef50acfeb20f0314d4d111aa2c575b6
TommyAndre <tommyab@egde.no>
2023-10-12 15:56:49 +0200

    AB#1147 add missing props for RadioGroup

--- packages/svelte/src/lib/components/Form/Radio/RadioGroup.svelte
+++ packages/svelte/src/lib/components/Form/Radio/RadioGroup.svelte
@@ -2,8 +2,17 @@
   import { writable } from 'svelte/store';
   import { setContext } from 'svelte';
 
+  export let legend = '';
+  export let description = '';
+  export let readOnly = false;
+  export let disabled = false;
+  export let error;
   export let value;
+  export let defaultValue;
+  export let required = false;
   export let inline = false;
+  export let size = 'medium';
+  export let hideLegend = false;
 
   const selectedValue = writable(value);
 
