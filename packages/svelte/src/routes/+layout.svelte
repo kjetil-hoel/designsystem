f0a6b8267d6351a107b1035293f479ae844bc339
kjetil-hoel <kjetilh@egde.no>
2023-11-22 15:43:07 +0100

    disabled func & styles, single selected remains object

--- packages/svelte/src/routes/+layout.svelte
+++ packages/svelte/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
-  import '@kjetil-hoel/design-system-tokens/brand/staf/tokens.css';
   import '@altinn/figma-design-tokens/dist/tokens.css';
+  import '@kjetil-hoel/design-system-tokens/brand/staf/tokens.css';
 </script>
 
 <slot />
