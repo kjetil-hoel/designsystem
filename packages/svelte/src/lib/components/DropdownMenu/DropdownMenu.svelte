ab31839800dc65abdbfa8427a3133d38d536e8a8
TobiasGunther <tobiasg@egde.no>
2024-01-17 14:33:47 +0100

    AB#4702 Added svelte files.

---
+++ packages/svelte/src/lib/components/DropdownMenu/DropdownMenu.svelte
@@ -0,0 +1,35 @@
+<script>
+  /**
+   * The placement of the dropdown.
+   * @type {'top' | 'right' | 'bottom' | 'left' | 'top-start' | 'top-end' | 'right-start' | 'right-end' | 'bottom-start' | 'bottom-end' | 'left-start' | 'left-end'}
+   */
+  export let placement = 'bottom-end';
+  /**
+   * The size of the dropdown.
+   * @type {'small' | 'medium' | 'large'}
+   */
+  export let size = 'medium';
+  /**
+   * Whether the dropdown is open or not
+   * @type {boolean}
+   */
+  export let open = false;
+  /**
+   * Element the popover anchors to.
+   * @type {Element | null}
+   */
+  export let anchorEl;
+  /**
+   * Callback function when dropdown closes.
+   * @type {() => void}
+   */
+  export let onClose;
+  /**
+   * Portals the floating element outside of the app root and into the body.
+   * @type {boolean}
+   */
+  export let portal;
+</script>
+
+<style>
+</style>
